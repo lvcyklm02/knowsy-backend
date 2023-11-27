711ba9d127f828fe777d57734dddc85cd5c32980
Julia Camacho <juliac123@gmail.com>
2023-11-26 23:00:24 -0500

    add locationResource to app

--- server/app.ts
+++ server/app.ts
@@ -2,9 +2,11 @@ import FriendConcept from "./concepts/friend";
 import PostConcept from "./concepts/post";
 import UserConcept from "./concepts/user";
 import WebSessionConcept from "./concepts/websession";
+import LocationResourceConcept from "./concepts/locationresource";
 
 // App Definition using concepts
 export const WebSession = new WebSessionConcept();
 export const User = new UserConcept();
 export const Post = new PostConcept();
 export const Friend = new FriendConcept();
+export const LocationResource = new LocationResourceConcept();
